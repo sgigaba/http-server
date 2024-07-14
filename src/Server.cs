3c281e5b9e5283bc69e9d9b889ed75ba70dee465
Samkelo Gigaba <samkelog@payspace.com>
2024-07-14 08:31:56 +0200

    Allow Client to connect to TCP Server and send data to client

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -8,3 +9,10 @@ Console.WriteLine("Logs from your program will appear here!");
  TcpListener server = new TcpListener(IPAddress.Any, 4221);
  server.Start();
  server.AcceptSocket(); // wait for client
+
+// AcceptSocket() will block untill a client is connected. Once a client has connected the print statement below will execute
+
+Console.WriteLine("Client Connectected");
+
+// AcceptSocket returns a socket you can use to send and receive data
+server.AcceptSocket().Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
