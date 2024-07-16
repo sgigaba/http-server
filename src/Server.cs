efa1bc21f8069895b6bf1aa00b3a8d7a0cae970c
Samkelo Gigaba <samkelog@payspace.com>
2024-07-16 15:24:12 +0200

    Respond with body

--- src/Server.cs
+++ src/Server.cs
@@ -8,21 +8,17 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
  TcpListener server = new TcpListener(IPAddress.Any, 4221);
  server.Start();
-// AcceptSocket() will block untill a client is connected. 
-// AcceptSocket returns a socket you can use to send and receive data
+// AcceptSocket() will block untill a client is connected. AcceptSocket returns a socket you can use to send and receive data
 var socket = server.AcceptSocket();
 
 string clientMessage = "HTTP/1.1 200 OK\r\n\r\n";
 Byte[] sendBytes = Encoding.ASCII.GetBytes(clientMessage);
-
 
 byte[] responseBytes = new byte[256];
 char[] responseChars = new char[256];
 
 int byteCount = socket.Receive(responseBytes);
 Encoding.ASCII.GetChars(responseBytes, 0, byteCount, responseChars, 0);
-
-Console.WriteLine(responseChars);
 
 string request = ""; 
 int i = 0;
@@ -34,16 +30,32 @@ while (responseChars[i] != '\r')
 }
 
 string requestTarget = request.Split(' ')[1];
-string requestResponse;
-switch(requestTarget)
+var endpoint = requestTarget.Split('/');
+
+string value="";
+string statusLine;
+string headerContentType ="";
+int headerContentLength = 0;
+switch(endpoint[1])
 {
-    case "/":
-        requestResponse = "200 OK";
+    case "":
+        statusLine = "200 OK";
+        break;
+    case "echo":
+        statusLine = "200 OK";
+        try{
+            value = endpoint[2];
+            headerContentType = "text/plain";
+            headerContentLength = value.Length;
+        }
+        catch(IndexOutOfRangeException){
+
+        }
         break;
     default:
-        requestResponse = "404 Not Found";
+        statusLine = "404 Not Found";
         break;
 }
 
-socket.Send(Encoding.ASCII.GetBytes($"HTTP/1.1 {requestResponse}\r\n\r\n"));
+socket.Send(Encoding.ASCII.GetBytes($"HTTP/1.1 {statusLine}\r\nContent-Type: {headerContentType}\r\nContent-Length: {headerContentLength}\r\n\r\n{value}"));
 socket.Close();
