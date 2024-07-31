f04b6fdca7c8c310ba26d61a2df41b3f68894dd4
Samkelo Gigaba <samkelog@payspace.com>
2024-07-31 06:39:54 +0200

    Refactor 3

--- src/Server.cs
+++ src/Server.cs
@@ -1,4 +1,5 @@
 using System.Buffers;
+using System.ComponentModel.DataAnnotations;
 using System.IO.Compression;
 using System.Net;
 using System.Net.Sockets;
@@ -22,41 +23,98 @@ Task ClientHandler(Socket socket)
     string clientRequest = Encoding.ASCII.GetString(clientBuffer);
 
     string[] requestLines = clientRequest.Split('\n');
-    string statusLine = requestLines.FirstOrDefault(_ => _.Contains("HTTP")); // Request Line
-    string requestMethod = statusLine.Split(' ')[0]; // Get or Post 
+    string statusLine = requestLines.First(_ => _.Contains("HTTP")); 
     string[] endpoint = statusLine.Split(' ')[1].Split('/');
-    string[] arg = Environment.GetCommandLineArgs();
 
-    if (requestLines != null)
+    switch(endpoint[1])
     {
-        string headerLine = requestLines.FirstOrDefault(_ => _.Contains("User-Agent"));
-        var userAgent = "";
-
-        if (headerLine != null)
-        {
-            userAgent = headerLine.Split(':')[1];
-        }
-        switch(endpoint[1])
-        {
-            case "":
-                DefaultEndpoint(socket);
-                break;
-            case "echo":
-                EchoEndpoint(socket, requestLines, endpoint);
-                break;
-            case "user-agent":
-                UserAgentEndpoint(socket, userAgent);
-                break;
-            case "files":
-                FileEndpoint(requestMethod, arg, endpoint, socket, requestLines);
-                break;
-            default:
-                NotFound(socket);
-                break;
-        }
+        case "":
+            DefaultEndpoint(socket);
+            break;
+        case "echo":
+            EchoEndpoint(socket, requestLines, endpoint);
+            break;
+        case "user-agent":
+            UserAgentEndpoint(socket, requestLines);
+            break;
+        case "files":
+            FileEndpoint(socket, requestLines, endpoint);
+            break;
+        default:
+            NotFound(socket);
+            break;
     }
- 
     return Task.CompletedTask;
+}
+
+void DefaultEndpoint(Socket socket) {
+    socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"));
+    socket.Close();
+} 
+
+void EchoEndpoint(Socket socket, string[] requestLines, string[] endpoint)
+{
+  try{
+        var encoding = requestLines.FirstOrDefault(_ => _.Contains("Accept-Encoding"));
+        SendResponse("200 OK","text/plain",endpoint[2], socket, encoding);
+    }
+    catch(IndexOutOfRangeException){
+        SendResponse("200 OK","text/plain","", socket, null);
+    }   
+}
+
+void FileEndpoint(Socket socket, string[] requestLines, string[] endpoint)
+{
+    string[] arg = Environment.GetCommandLineArgs();
+    string requestMethod = requestLines.First(_ => _.Contains("HTTP")).Split(' ')[0]; 
+    switch(requestMethod)
+    {
+        case "GET":
+            if (File.Exists(arg[2] + endpoint[2])){
+                var body = File.ReadAllText(arg[2] + endpoint[2]);
+                socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 200 Ok\r\nContent-Type: application/octet-stream\r\nContent-Length: {body.Length}\r\n\r\n{body}"));
+                socket.Close();
+            }
+            else{
+                socket.Send(Encoding.ASCII.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+                socket.Close();
+            }
+        break;
+
+        case "POST":
+            var newFile = endpoint[2].Split(' ')[0];
+            string contentLength = requestLines.First(_ => _.Contains("Content-Length")).Split(":")[1].Trim();
+            int finalLength = 0;
+
+            Int32.TryParse(contentLength, out finalLength);
+            var data = requestLines.Last().Substring(0, finalLength);
+
+            using (StreamWriter sw = File.CreateText(arg[2] + newFile)){
+                sw.Write(data);
+            }
+            socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 201 Created\r\n\r\n"));
+            socket.Close();
+        break;                                       
+    }
+}
+
+void NotFound(Socket socket)
+{
+    socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 404 Not Found\r\n\r\n"));
+    socket.Close();
+}
+
+void UserAgentEndpoint(Socket socket, string[] requestLines)
+{
+    string? headerLine = requestLines.FirstOrDefault(_ => _.Contains("User-Agent"));
+    var userAgent = "";
+
+    if (headerLine != null)
+    {
+        userAgent = headerLine.Split(':')[1];
+    }
+    socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Trim().Length}\r\n\r\n{userAgent.Trim()}"));
+    socket.Close();
 }
 
 void SendResponse(string statusLine,string? headerContentType, string? body, Socket socket, string? encoding)
@@ -86,63 +144,3 @@ void SendResponse(string statusLine,string? headerContentType, string? body, Soc
     }
 }
 
-void DefaultEndpoint(Socket socket) => SendResponse("200 OK","text/plain","", socket, null);
-
-void EchoEndpoint(Socket socket, string[] requestLines, string[] endpoint)
-{
-  try{
-        var encoding = requestLines.FirstOrDefault(_ => _.Contains("Accept-Encoding"));
-        SendResponse("200 OK","text/plain",endpoint[2], socket, encoding);
-    }
-    catch(IndexOutOfRangeException){
-        SendResponse("200 OK","text/plain","", socket, null);
-    }   
-}
-
-void FileEndpoint(string requestMethod, string[] arg, string[] endpoint, Socket socket, string[] requestLines)
-{
-    switch(requestMethod)
-    {
-        case "GET":
-            try{
-                if (File.Exists(arg[2] + endpoint[2]))
-                {
-                    var body = File.ReadAllText(arg[2]+endpoint[2]);
-                    SendResponse("200 OK","application/octet-stream", body, socket, null);                                                 
-                }
-                else{
-                    socket.Send(Encoding.ASCII.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
-                    socket.Close();
-                }
-            }
-            catch (FileNotFoundException) 
-            {
-                Console.WriteLine("File Not Found");
-            }
-        break;
-
-        case "POST":
-            var newFile = endpoint[2].Split(' ')[0];
-            string contentLength = requestLines.FirstOrDefault(_ => _.Contains("Content-Length")).Split(":")[1].Trim();
-            int finalLength = 0;
-            Int32.TryParse(contentLength, out finalLength);
-            var data = requestLines.Last().Substring(0, finalLength);
-            Console.WriteLine(data);
-            using (StreamWriter sw = File.CreateText(arg[2] + newFile)){
-                sw.Write(data);
-            }
-            SendResponse("201 Created","","" , socket, null);                                                 
-            socket.Close();
-        break;                                       
-    }
-}
-
-void NotFound(Socket socket)
-{
-    SendResponse("404 Not Found","", "", socket, null);
-}
-
-void UserAgentEndpoint(Socket socket, string userAgent)
-{
-    SendResponse("200 OK","text/plain",userAgent.Trim(), socket, null);
-}
